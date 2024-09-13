a3ee306f32c46a18a66258f5bd9fb47e4357fea7
SlayersDevClub <68347537+SlayersDevClub@users.noreply.github.com>
2024-09-13 19:15:31 -0400

    moving files

--- Assets/CardFramework/Scripts/Component/Card.cs
+++ Assets/CardFramework/Scripts/Component/Card.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 public class Card : MonoBehaviour 
-{	public string cardName;
+{	
+	public string cardName;
 	public string TexturePath { get; set; }
 	
 	public string SourceAssetBundlePath { get; set; }
